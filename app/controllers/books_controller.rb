d8db8d7e81b37dfa19e9265582b97b51caef7df9
Yuji <mtcook3754@gmail.com>
2019-05-03 19:27:40 +0900

    バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,18 @@ def new
   end
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to @book, notice: "書籍を登録しました。"
+    if @book.save
+      redirect_to @book, notice: "書籍を登録しました。"
+    else
+      render :new
+    end
   end
+def show
+  @book = Book.find(params[:id])
+end
+
+
+
   private
   def book_params
     params.require(:book).permit(:title, :price, :publish_date, :description)
