0d8bb27e29e6192eb193639351aa0f13017c5a50
Yuji <mtcook3754@gmail.com>
2019-05-03 17:37:30 +0900

    booksのクリエイト完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,17 @@ class BooksController < ApplicationController
 def index
   @books = Book.all
 end
+
+def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to @book, notice: "書籍を登録しました。"
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :price, :publish_date, :description)
+  end
 end
